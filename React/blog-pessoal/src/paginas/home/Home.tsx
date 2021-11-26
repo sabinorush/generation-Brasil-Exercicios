a968c243b57db747874c2c75b2fa8876d6d1b190
sabinorush <gu.sabino@hotmail.com>
2021-11-26 15:18:55 -0300

    First commit react :tada:

---
+++ React/blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <>
+      <h1 className="titulo">Home</h1>
+      <img src="https://i0.wp.com/naoseinada.com.br/wp-content/uploads/2021/01/scottpilgrim.jpg?ssl=1" alt="scott pilgrim" className="img" />
+    </>
+  );
+
+}
+
+export default Home;
